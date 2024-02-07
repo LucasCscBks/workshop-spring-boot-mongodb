a3bb4113ccf794ae58eea66222bb8bd6d097840e
LucasCscBks <luke100191@gmail.com>
2024-02-07 16:01:33 -0300

    FindById User

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -1,12 +1,14 @@
 package com.educandoweb.course.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.domain.User;
 import com.educandoweb.course.repository.UserRepository;
+import com.educandoweb.course.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,15 @@ public class UserService {
     
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    @SuppressWarnings("null")
+    public User findById(String id) {
+        Optional<User> user = repo.findById(id);
+        if (user.isPresent()) {
+            return user.get();
+        } else {
+            throw new ObjectNotFoundException("Object not found");
+        }        
     }
 }
