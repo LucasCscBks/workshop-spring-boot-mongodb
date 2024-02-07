4eba27c9bb4b50ab0fe63ec8726f48963192d0fd
LucasCscBks <luke100191@gmail.com>
2024-02-07 16:25:39 -0300

    Update user put

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.domain.User;
+import com.educandoweb.course.dto.UserDTO;
 import com.educandoweb.course.repository.UserRepository;
 import com.educandoweb.course.services.exception.ObjectNotFoundException;
 
@@ -28,5 +29,32 @@ public class UserService {
         } else {
             throw new ObjectNotFoundException("Object not found");
         }        
+    }
+
+    @SuppressWarnings("null")
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    @SuppressWarnings("null")
+    public void delete(String id) {
+        findById(id);
+        repo.deleteById(id);
+    }
+
+    @SuppressWarnings("null")
+    public User update(User obj) {
+        Optional<User> newObj = repo.findById(obj.getId());
+        updateData(newObj.get(), obj);
+        return repo.save(newObj.get());
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
